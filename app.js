47468f121cbde564f2605d57538bce52536221b0
3king <tre@icandylabs.com>
2015-06-24 09:56:44 -0500

    Switched to hello dolly

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello baby');
+  res.send('hello dolly');
 });
  
 app.listen(process.env.PORT || 5000);
