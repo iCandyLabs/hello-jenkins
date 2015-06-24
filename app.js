142853aca4aff72ca849fbb56d100de0e514a781
root <root@ip-172-31-37-29.us-west-2.compute.internal>
2015-06-24 14:36:40 +0000

    reverted chanages

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello icandylabs');
+  res.send('hello jenkins');
 });
  
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 5000);
  
 module.exports = app;
