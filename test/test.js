65df22c3d4a3fda1c5c9db0ca585f6fa332537ad
3king <tre@icandylabs.com>
2015-06-24 09:52:44 -0500

    merge fix

--- test/test.js
+++ test/test.js
@@ -3,10 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-<<<<<<< HEAD
     request(app).get('/').expect('hello baby', done);
-=======
-    request(app).get('/').expect('hello Jenkins', done);
->>>>>>> e00a79375e77128fb31cd206eaac07e312133588
   });
 });
